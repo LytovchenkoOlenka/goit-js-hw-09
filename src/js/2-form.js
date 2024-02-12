47b4dd81927347385c30f48d876666ce407b1561
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2024-02-12 15:59:18 +0100

    HW

---
+++ src/js/2-form.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const feedbackForm = document.querySelector('.feedback-form');
+const emailInput = feedbackForm.querySelector('[name="email"]');
+const messageInput = feedbackForm.querySelector('[name="message"]');
+
+feedbackForm.addEventListener('input', function () {
+  const currentState = {
+    email: emailInput.value.trim(),
+    message: messageInput.value.trim(),
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(currentState));
+});
+
+const storedState = JSON.parse(
+  localStorage.getItem('feedback-form-state') || {}
+);
+
+emailInput.value = storedState.email || '';
+messageInput.value = storedState.message || '';
+
+feedbackForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const currentData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  if (!currentData.email || !currentData.message) {
+    alert('Please fill in both fields of the form.');
+    return;
+  }
+
+  console.log(currentData);
+  localStorage.removeItem('feedback-form-state');
+  feedbackForm.reset();
+});
